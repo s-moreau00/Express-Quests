7b9a6f7b0b9713505cf9124c3dd4f4f62a3345ba
s-moreau00 <moreau.sylvain6969@gmail.com>
2024-01-29 12:02:09 +0100

    challenge express1.02

--- src/app.js
+++ src/app.js
@@ -4,7 +4,11 @@ const app = express();
 
 const movieControllers = require("./controllers/movieControllers");
 
+app.get("/", movieControllers.getApp);
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.get("/api/users", movieControllers.getUsers);
+app.get("/api/users/:id", movieControllers.getUserById);
+
 
 module.exports = app;
