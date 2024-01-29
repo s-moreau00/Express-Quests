1ce416df394eeb3ebb68f03bb6c248fffb39c0a3
s-moreau00 <moreau.sylvain6969@gmail.com>
2024-01-29 20:34:55 +0100

    new commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
 const movieControllers = require("./controllers/movieControllers");
 
 app.get("/", movieControllers.getApp);
@@ -9,6 +10,9 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", movieControllers.getUsers);
 app.get("/api/users/:id", movieControllers.getUserById);
+app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/users", movieControllers.postNewUser);
 
+//app corespond à route
 
 module.exports = app;
